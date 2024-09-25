3c25ede559fb25d2801e0d53e9b74983ba275e11
Padmasri Ambati <143399502+PADMASRIAMBATI@users.noreply.github.com>
2024-09-25 21:25:29 +0530

    Add files via upload

---
+++ 3.prime.py
@@ -0,0 +1,24 @@
+# Program to Check Prime Number in Python Programming
+def check_prime(n):
+    x=False
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else:
+        f+
+        
+            if n % i == 0:
+                x=False
+                break
+                
+            else:
+                 x=True
+        return x
+n=int(input("Enter number:"))
+if check_prime(n):
+    print(n,"is a prime number")
+else:
+    print(n,"is not a prime number") 
